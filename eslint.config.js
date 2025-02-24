5f411040ca546d814ac661268fe869a53bfddf2b
Vladimir Ilic <vladimir.ilic101@gmail.com>
2025-02-24 15:32:32 +0100

    Angular ESLint & Prettier Configuration improvement to properly lint templates

--- eslint.config.js
+++ eslint.config.js
@@ -36,7 +36,11 @@ module.exports = tseslint.config(
   },
   {
     files: ['**/*.html'],
-    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      eslintPluginPrettierRecommended
+    ],
     rules: {},
   },
 )
